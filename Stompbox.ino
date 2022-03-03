0ed0ad2747b084ec76d818b73e8f18d2d5aee8d7
danedebugs <73204753+danedebugs@users.noreply.github.com>
2022-03-03 11:09:39 -0800

    Update Stompbox.ino

--- Stompbox.ino
+++ Stompbox.ino
@@ -69,16 +69,16 @@ pinMode(relaypass,OUTPUT);
 
  //turn on the timer clock in the power management controller
   pmc_set_writeprotect(false);
-  pmc_enable_periph_clk(ID_TC4);
+  pmc_enable_periph_clk(ID_TC4); //Timer clock 4 ISR-> TC1 
 
   //we want wavesel 01 with RC 
   TC_Configure(TC1,1,TC_CMR_WAVE|TC_CMR_WAVSEL_UP_RC|TC_CMR_TCCLKS_TIMER_CLOCK2);
-  TC_SetRC(TC1, 1, 238); // sets <> 44.1 Khz interrupt rate
-  TC_Start(TC1, 1);
+  TC_SetRC(TC1, 1, 238); // sets <> 44.1 Khz interrupt rate 10.5Mhz/44.1Khz = 238.09
+  TC_Start(TC1, 1); //start timer
  
   // enable timer interrupts on the timer
-  TC1->TC_CHANNEL[1].TC_IER=TC_IER_CPCS;
-  TC1->TC_CHANNEL[1].TC_IDR=~TC_IER_CPCS;
+  TC1->TC_CHANNEL[1].TC_IER=TC_IER_CPCS; // IER = interrupt enable register
+  TC1->TC_CHANNEL[1].TC_IDR=~TC_IER_CPCS; // IDR = interrupt disable register
  
   //Enable the interrupt in the nested vector interrupt controller 
   //TC4_IRQn where 4 is the timer number * timer channels (3) + the channel number 
@@ -86,7 +86,7 @@ pinMode(relaypass,OUTPUT);
   NVIC_EnableIRQ(TC4_IRQn);
   
   //ADC Configuration
-  ADC->ADC_MR |= 0x80;   // DAC in free running mode.
+  ADC->ADC_MR |= 0x80;   // ADC in free running mode.
   ADC->ADC_CR=2;         // Starts ADC conversion.
   ADC->ADC_CHER=0x1CC0;  // Enable ADC channels 0,1,8,9 and 10  
 
@@ -107,7 +107,7 @@ void loop()
   POT2=ADC->ADC_CDR[12];                 // read data from ADC10 (potwntiometer) 
 }
 
-void TC4_Handler()
+void TC4_Handler() //44.1KHz timer interrupt
 {
   if(digitalRead(effect3)) // reverb
   {
